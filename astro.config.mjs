075836708b35e52ce761df12d65ffe8a90b51fba
aprinlyn <lyan.apri97@gmail.com>
2026-04-05 22:41:35 +0700

    add config cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,16 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yourname.dev',
   integrations: [mdx(), sitemap(), react()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
