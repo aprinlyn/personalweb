58799371863699b59186b1d751f9d2a2b81f3a58
aprinlyn <lyan.apri97@gmail.com>
2026-04-08 08:52:59 +0700

    update host

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yourname.dev',
+  site: 'https://aliyantoko.io',
   integrations: [mdx(), sitemap(), react()],
 
   vite: {
